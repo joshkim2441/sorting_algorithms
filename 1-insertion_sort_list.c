8171018d41628efbf83484ef582a13f6795defe5
joshkim2441 <joshkim2441@gmail.com>
2024-01-22 23:43:47 +0300

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,12 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of
- * integers in ascending order
- * @list: The doubly-linked list to be sorted
- * Return: The sorted list
-*/
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *curr;
-
-	if (*list == NULL || (*list)->next == NULL)
-	{ /*List is empty or has only one element, no sorting needed*/
-		return;
-	}
-
-	for (curr = (*list)->next; curr != NULL; curr = curr->next)
-	{
-		/*Insert current node in its sorted position*/
-		while (curr->prev != NULL && curr->n < curr->prev->n)
-		{
-			nodes_swap(list, curr->prev, curr);
-			print_list(*list); /*Print list after each swap*/
-		}
-	}
-}
-
-/**
  * nodes_swap - A helper function to swap two nodes in the list
  * @node1: Node to swap with node2
  * @node2: Node to swap with node1
  * @list: The node list
  * Return: A pointer to the node inserted
-*/
+ */
 void nodes_swap(listint_t **list, listint_t *node1, listint_t *node2)
 {
 	if (node1 == node2)
@@ -58,4 +32,30 @@ void nodes_swap(listint_t **list, listint_t *node1, listint_t *node2)
 	node2->prev = node1->prev;
 	node1->prev = node2;
 	node2->next = node1;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of
+ * integers in ascending order
+ * @list: The doubly-linked list to be sorted
+ * Return: The sorted list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr;
+
+	if (*list == NULL || (*list)->next == NULL)
+	{ /*List is empty or has only one element, no sorting needed*/
+		return;
+	}
+
+	for (curr = (*list)->next; curr != NULL; curr = curr->next)
+	{
+		/*Insert current node in its sorted position*/
+		while (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			nodes_swap(list, curr->prev, curr);
+			print_list(*list); /*Print list after each swap*/
+		}
+	}
 }
