87493e459597b8beecf1b0047b5515e4f926f484
joshkim2441 <joshkim2441@gmail.com>
2024-01-23 00:10:40 +0300

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,31 +7,25 @@
  * @list: The node list
  * Return: A pointer to the node inserted
  */
-void nodes_swap(listint_t **list, listint_t *node1, listint_t *node2)
+void nodes_swap(listint_t **list, listint_t **node1, listint_t *node2)
 {
-	if (node1 == node2)
+	(*node1)->next = node2->next;
+	if (node2->next)
 	{
-		return;
+		node2->next->prev = *node1;
 	}
-
-	if (node1->prev != NULL)
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev)
 	{
-		node1->prev->next = node2;
+		(*node1)->prev->next = node2;
 	}
 	else
 	{
 		*list = node2;
 	}
-
-	if (node2->next != NULL)
-	{
-		node2->next->prev = node1;
-	}
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
@@ -42,20 +36,22 @@ void nodes_swap(listint_t **list, listint_t *node1, listint_t *node2)
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr;
+	listint_t *curr, *embed, *temp;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 	{ /*List is empty or has only one element, no sorting needed*/
 		return;
 	}
 
-	for (curr = (*list)->next; curr != NULL; curr = curr->next)
+	for (curr = (*list)->next; curr != NULL; curr = temp)
 	{
+		temp = curr->next;
+		embed = curr->prev;
 		/*Insert current node in its sorted position*/
-		while (curr->prev != NULL && curr->n < curr->prev->n)
+		while (embed != NULL && curr->n < embed->n)
 		{
-			nodes_swap(list, curr->prev, curr);
-			print_list(*list); /*Print list after each swap*/
+			nodes_swap(list, &embed, curr);
+			print_list((const listint_t *)*list); /*Print list after each swap*/
 		}
 	}
 }
